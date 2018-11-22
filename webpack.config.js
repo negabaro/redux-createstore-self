19619aa846883058e1c3edee4b59bda35d33c9e5
negabaro kim <negabaro@naver.com>
2018-11-22 21:19:54 +0000

    react hello world出ることまで確認

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    devServer: {
+      host: '0.0.0.0',
+      disableHostCheck: true
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts|\.tsx|\.js|\.jsx$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ]
+    }
+  };
